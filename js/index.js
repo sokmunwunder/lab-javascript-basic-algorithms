2dc9e5f6c9e2ec698eb109302f2f21eaa3f5e47a
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-10-24 18:01:27 +0200

    Iterations 1 to 3 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
-// Iteration 1: Names and Input
+
+/* Iteration 1: Name and Input*/
+/*Iteration 1: Names and Input
+1.1 Create a variable `hacker1` with the driver's name.
+1.2 Print `"The driver's name is XXXX"`.
+1.3 Create a variable `hacker2` with the navigator's name.
+1.4 Print `"The navigator's name is YYYY"`.
+*/
+let hacker1 = "Mary";
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Johnny";
+console.log(`The navigator's name is ${hacker2}.`);
+
+/* Iteration 2: Conditionals
+2.1. Depending on which name is longer, print:
+- The driver has the longest name, it has XX characters. or
+- It seems that the navigator has the longest name, it has XX characters. or
+- Wow, you both have equally long names, XX characters!.
+*/
+
+if(hacker1.length > hacker2.length){console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);}
+else if (hacker1.length < hacker2.length){console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);}
+else if (hacker1.length === hacker2.length){console.log(`Wow you both have equally long names, ${hacker1.length || hacker2.length} characters!`);}
 
 
-// Iteration 2: Conditionals
+/* Iteration 3: Loops */
+/* Iteration 3.1: Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N" */
 
+let hacker1UpperCase = hacker1.toUpperCase();
+console.log(`${hacker1UpperCase.charAt(0)}`+" "+`${hacker1UpperCase.charAt(1)}`+ " " + `${hacker1UpperCase.charAt(2)}` + " "+`${hacker1UpperCase.charAt(3)}`);
 
-// Iteration 3: Loops
+/* Iteration 3:2: Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ" */
+
+console.log(`${hacker1.charAt(3)}`+`${hacker1.charAt(2)}` + `${hacker1.charAt(1)}` + `${hacker1.charAt(0)}`);
+
+/* Iteration 3:3: Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name? */
+
+if (hacker1.localeCompare(hacker2) >0 ){console.log(`${hacker1}´s name goes first`);}
+else if(hacker1.localeCompare(hacker2) <0){console.log(`Yo,${hacker2} goes first definitely`);}
+else{console.log("What ?! You both have the same name?");}
